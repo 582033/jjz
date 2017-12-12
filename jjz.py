08dc7c6b4de777d96f67a22eaf60ebbb08c2423b
Yan Jiang <j582033@163.com>
2017-12-12 10:33:39 +0800

    精简部分代码

--- jjz.py
+++ jjz.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import base64
 import requests
 import time
-import json
 import os
 from libs import Log, Message
 
-
-
-
 class jjz:
     def __init__(self):
-        self.cafile = './charles-ssl-proxying-certificate.pem'
-        self.cookie = {
-             'JSESSIONID' : '0687B4603F1298EADD52B05945D6BB0C',
-             'UM_distinctid' : '15ad9489fb5db-0d08f425adab3b-4f20076b-38400-15ad9489fb71f1',
-        }
+        self.sign_url = 'https://enterbj.zhongchebaolian.com/enterbj/platform/enterbj/curtime_03'
 
     def _request(self, url, headers={}):
         requests.packages.urllib3.disable_warnings()
@@ -27,23 +18,7 @@ class jjz:
         return r
 
     def check302(self):
-        url = 'https://enterbj.zhongchebaolian.com/enterbj/platform/enterbj/curtime_03'
-        headers = {
-            'Host' : 'enterbj.zhongchebaolian.com',
-            'Connection' : 'keep-alive',
-            'Pragma' : 'no-cache',
-            'Cache-Control' : 'no-cache',
-            'Upgrade-Insecure-Requests' : '1',
-            'User-Agent' : 'Mozilla/5.0 (Linux; Android 7.0; MI 5 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 Mobile Safari/537.36',
-            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Referer': 'https://enterbj.zhongchebaolian.com/enterbj/platform/enterbj/toVehicleType',
-            'Accept-Encoding' : 'gzip, deflate',
-            'Accept-Language' : 'zh-CN,en-US;q=0.8',
-            'Cookie' : 'JSESSIONID=5F9CAA1A713B817DCD8C03D730C1405E; UM_distinctid=15ec7ad338672-0758eb2f67658a-12797d23-38400-15ec7ad338716a; CNZZDATA1260761932=1788518962-1499394672-https%253A%252F%252Fenterbj.zhongchebaolian.com%252F%7C1507934398',
-            'X-Requested-With' : 'com.zcbl.bjjj_driving',
-
-        }
-        r = self._request(url, headers)
+        r = self._request(self.sign_url)
         status_code = r.status_code
         headers = r.headers
 
@@ -73,5 +48,5 @@ if res:
     msg.push('%s 可以办理进京证啦~' % str(now))
     #msg.push('可以办理进京证啦~', 4)
 else:
-    log.debug('不能办理')
+    #log.debug('不能办理')
     print('不能办理')
